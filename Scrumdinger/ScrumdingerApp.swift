50473edd4350d40c25ed1813dc4d0586c846696e
Jonathan Alvares <alvaresjonathan599@gmail.com>
2022-09-28 16:03:15 +0530

    Leant the basics of SwiftUI

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by JONATHAN ALVARES on 28/09/22.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MeetingView()
+        }
+    }
+}
