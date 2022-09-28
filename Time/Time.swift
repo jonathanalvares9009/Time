cc1bd0ef3b2e6b57cf9758a0b95e9f2a4bdd85e0
Jonathan Alvares <alvaresjonathan599@gmail.com>
2022-09-28 19:04:20 +0530

    Learnt how to use views in SwiftUI

---
+++ Time/Time.swift
@@ -0,0 +1,40 @@
+/*
+See LICENSE folder for this sample’s licensing information.
+*/
+
+import SwiftUI
+
+@main
+struct Time: App {
+    @StateObject private var store = ScrumStore()
+    @State private var errorWrapper: ErrorWrapper?
+    
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                Text("Hello World")
+//                ScrumsView(scrums: $store.scrums) {
+//                    Task {
+//                        do {
+//                            try await ScrumStore.save(scrums: store.scrums)
+//                        } catch {
+//                            errorWrapper = ErrorWrapper(error: error, guidance: "Try again later.")
+//                        }
+//                    }
+//                }
+            }
+            .task {
+                do {
+                    store.scrums = try await ScrumStore.load()
+                } catch {
+                    errorWrapper = ErrorWrapper(error: error, guidance: "Scrumdinger will load sample data and continue.")
+                }
+            }
+            .sheet(item: $errorWrapper, onDismiss: {
+                store.scrums = DailyScrum.sampleData
+            }) { wrapper in
+                ErrorView(errorWrapper: wrapper)
+            }
+        }
+    }
+}
